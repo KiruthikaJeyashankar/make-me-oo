e4bddd1f01f51684488c76353c08b9ee9c401a9f
Kiruthika J <kiruthikavjshankar@gmail.com>
2021-03-27 14:14:36 +0530

    Refactor. Changed the code to follow OOP | [ Kiruthika ]

--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -1,27 +1,54 @@
 package org.oop;
 
+import static java.lang.Math.atan2;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
 public class Point {
-    private double x;
-    private double y;
+    private final double xPosition;
+    private final double yPosition;
 
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
 
     public Point(double x, double y) {
-        this.x = x;
-        this.y = y;
+        this.xPosition = x;
+        this.yPosition = y;
+    }
+
+    public double distance(Point point) {
+        if (this == point)
+            return 0.0;
+
+        Point differenceVector = getDifferenceVector(this , point);
+
+        return sqrt(square(differenceVector.xPosition) + square(differenceVector.yPosition));
+    }
+
+    public double direction(Point point) {
+        Point differenceVector = getDifferenceVector(this , point);
+
+        return atan2(differenceVector.yPosition, differenceVector.xPosition);
+    }
+
+    private Point getDifferenceVector(Point from , Point to) {
+        double xDistance = to.xPosition - from.xPosition;
+        double yDistance = to.yPosition - from.yPosition;
+
+        return new Point(xDistance, yDistance);
+    }
+
+    private double square(double value) {
+        return pow(value, 2);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+
+        return this.xPosition == ((Point) obj).xPosition && this.yPosition == ((Point) obj).yPosition;
+
     }
 }
--- src/test/java/org/oop/PointTest.java
+++ src/test/java/org/oop/PointTest.java
@@ -1,16 +1,17 @@
 package org.oop;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.oop.DistanceAndDirectionCalculator.direction;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class DistanceAndDirectionCalculatorTest {
+class PointTest {
 
     @Test
     void twoPointsWithSameXAndYCoordinatesShouldHaveADistanceOfZero() {
         Point origin = new Point(0, 0);
-        Assertions.assertEquals(0, DistanceAndDirectionCalculator.distance(origin, origin));
+
+        assertEquals(0, origin.distance(origin));
+        assertEquals(0 , origin.distance(new Point(0 , 0)));
     }
 
     @Test
@@ -19,8 +20,8 @@ public class DistanceAndDirectionCalculatorTest {
         Point point1 = new Point(1, 0);
         Point point2 = new Point(0, 1);
 
-        Assertions.assertEquals(1, DistanceAndDirectionCalculator.distance(origin, point1));
-        Assertions.assertEquals(1, DistanceAndDirectionCalculator.distance(origin, point2));
+        assertEquals(1, origin.distance(point1));
+        assertEquals(1, origin.distance(point2));
     }
 
     @Test
@@ -28,36 +29,41 @@ public class DistanceAndDirectionCalculatorTest {
         Point point1 = new Point(1, 0);
         Point point2 = new Point(-1, 0);
 
-        Assertions.assertEquals(2, DistanceAndDirectionCalculator.distance(point1, point2));
+        assertEquals(2, point1.distance(point2));
     }
+
 
     @Test
     void originAndPointOnPostiveXAxisShouldBeZeroRadiansAway() {
+
         Point origin = new Point(0, 0);
         Point point1 = new Point(1, 0);
         Point point2 = new Point(3, 0);
 
-        Assertions.assertEquals(0, direction(origin, point1));
-        Assertions.assertEquals(0, direction(origin, point2));
+        assertEquals(0, origin.direction(point1));
+        assertEquals(0, origin.direction(point2));
     }
 
     @Test
     void originAndPointOnNegativeXAxisShouldBePiRadiansAway() {
+
         Point origin = new Point(0, 0);
         Point point1 = new Point(-1, 0);
         Point point2 = new Point(-3, 0);
 
-        Assertions.assertEquals(Math.PI, direction(origin, point1));
-        Assertions.assertEquals(Math.PI, direction(origin, point2));
+        assertEquals(Math.PI, origin.direction(point1));
+        assertEquals(Math.PI, origin.direction(point2));
+
     }
 
     @Test
     void originAndPointOnYAxisShouldBeHalfPiRadiansAway() {
+
         Point origin = new Point(0, 0);
         Point point1 = new Point(0, 1);
         Point point2 = new Point(0, 3);
 
-        Assertions.assertEquals(Math.PI / 2, direction(origin, point2));
-        Assertions.assertEquals(Math.PI / 2, direction(origin, point1));
+        assertEquals(Math.PI / 2, origin.direction(point1));
+        assertEquals(Math.PI / 2, origin.direction(point2));
     }
 }
